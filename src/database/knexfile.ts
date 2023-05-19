33872d76317103f912678eb2735d9ee8837078ca
Heichenko Viktor <viktor.heichenko@softengi.com>
2023-05-20 00:00:56 +0300

    migrations work

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,16 +1,13 @@
-
 import { resolve } from 'path'
-import {config} from 'dotenv'
+import { config } from 'dotenv'
+
+import { Knex } from 'knex'
+
+const __dirname = resolve()
+
 config({ path: resolve(__dirname, '../../.env') })
 
-console.log({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-})
-
-module.exports = {
+const dbConfig: Knex.Config = {
   client: 'pg',
   connection: {
     host: process.env.PG_HOST,
@@ -22,3 +19,4 @@ module.exports = {
     directory: resolve('./migrations'),
   },
 }
+export default dbConfig
